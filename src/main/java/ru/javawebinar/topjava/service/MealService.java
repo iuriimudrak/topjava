24bbb2d17520f50c7e505fa1311d106a6e0b78ff
iuriimudrak <mudrak.yura.1991@gmail.com>
2020-10-14 23:02:50 +0300

    Implement MealService

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,41 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    @Autowired
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Integer userId, Meal meal) {
+        return repository.save(userId, meal);
+    }
+
+    public Meal get(int userId, int id) {
+        return checkNotFoundWithId(repository.get(userId, id), id);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public void update(int userId, Meal meal) {
+        checkNotFoundWithId(repository.save(userId, meal), meal.getId());
+    }
+
+    public void delete (int userId, int id) {
+        checkNotFoundWithId(repository.delete(userId, id), id);
+    }
 }
