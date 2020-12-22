3d1711d89e734782756ac5341ab0a1bcb7d79b81
iuriimudrak <mudrak.yura.1991@gmail.com>
2020-12-23 00:16:44 +0300

    8 07 ajax datatables

---
+++ src/main/webapp/resources/js/topjava.users.js
@@ -0,0 +1,45 @@
+var ctx;
+
+// $(document).ready(function () {
+$(function () {
+    // https://stackoverflow.com/a/5064235/548473
+    ctx = {
+        ajaxUrl: "admin/users/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "name"
+                },
+                {
+                    "data": "email"
+                },
+                {
+                    "data": "roles"
+                },
+                {
+                    "data": "enabled"
+                },
+                {
+                    "data": "registered"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    };
+    makeEditable();
+});
